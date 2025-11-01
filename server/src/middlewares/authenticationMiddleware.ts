9c9e2aebb654824a0e3d15e4dc033ec3e3cc4903
Stefano Gaspardone <s337201@studenti.polito.it>
2025-11-01 10:35:39 +0100

    feat: some more structure added

---
+++ server/src/middlewares/authenticationMiddleware.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express';
+
+// export const isAuthenticated = (allowedRoles: UserRole[] = []) => {
+//     return async (req: Request, res: Response, next: NextFunction) => {
+//         try {
+
+//         } catch(error) {
+//             next(error);
+//         }
+//     }
+// }
