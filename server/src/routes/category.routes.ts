746507e906336c6dc9d1e37ee12ce2142fc7cce6
Stefano Gaspardone <s337201@studenti.polito.it>
2025-11-06 17:26:23 +0100

    fix: scripts folder inside root folder

--- server/src/routes/category.routes.ts
+++ server/src/routes/category.routes.ts
@@ -1,8 +1,10 @@
 import { categoryController } from '@controllers/CategoryController';
+import { UserType } from '@daos/UserDAO';
+import { authMiddleware } from '@middlewares/authenticationMiddleware';
 import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', categoryController.findAllCategories); // TODO auth middleware
+router.get('/', authMiddleware([UserType.CITIZEN]), categoryController.findAllCategories);
 
 export const categoryRouter = router;
