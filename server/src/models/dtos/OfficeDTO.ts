72db49818d71f284be466732a17a11ca9bc22658
Federico Ferrari <fedef306@gmail.com>
2025-11-08 17:31:25 +0100

    fix: userTypes and Office

--- server/src/models/dtos/OfficeDTO.ts
+++ server/src/models/dtos/OfficeDTO.ts
@@ -1,20 +1,20 @@
-import {MunicipalityRoleDAO} from "@daos/MunicipalityRoleDAO";
+import {OfficeDAO} from "@daos/OfficeDAO";
 import {MapUserDAOtoDTO, UserDTO} from "@dtos/UserDTO";
 import {CategoryDTO, createCategoryDTO} from "@dtos/CategoryDTO";
 
 
-export class MunicipalityRoleDTO {
+export class OfficeRoleDTO {
     id: number;
     name: string;
-    categories: CategoryDTO[];
+//    categories: CategoryDTO[];
 //    users: UserDTO[];
 }
 
-export const mapRolesDAOtoDTO = (role: MunicipalityRoleDAO): MunicipalityRoleDTO => {
+export const mapRolesDAOtoDTO = (role: OfficeDAO): OfficeRoleDTO => {
     return {
         id: role.id,
         name: role.name,
-        categories: role.categories.map(createCategoryDTO),
+//        categories: role.categories.map(createCategoryDTO),
 //        users: role.users.map(MapUserDAOtoDTO),
     };
 }
