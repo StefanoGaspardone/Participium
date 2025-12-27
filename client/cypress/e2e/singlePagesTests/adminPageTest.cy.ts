636fb0e0db4d3d90b34060358edee112ac3f1c42
Giacomo Scorza <giacomoscorza@gmail.com>
2025-12-27 16:10:31 +0100

    completed tests for new admin page features

--- client/cypress/e2e/singlePagesTests/adminPageTest.cy.ts
+++ client/cypress/e2e/singlePagesTests/adminPageTest.cy.ts
@@ -46,12 +46,35 @@ const stubAdminPageData = () => {
 
     cy.intercept('GET', '/api/categories', {
         statusCode: 200,
-        body: { categories: [
-            { id: 1, name: 'Road' },
-            { id: 2, name: 'Lighting' },
-            { id: 3, name: 'Trash' },
-        ]},
+        body: {
+            categories: [
+                { id: 1, name: 'Road' },
+                { id: 2, name: 'Lighting' },
+                { id: 3, name: 'Trash' },
+            ]
+        },
     }).as('getCategories');
+
+    cy.intercept('GET', '/api/users/tsm', {
+        statusCode: 200,
+        body: [
+            {
+                id: 1, firstName: 'Carmine', lastName: 'Conte', email: 'tsm1@part.se', username: 'tsm1', userType: 'TECHNICAL_STAFF_MEMBER', offices: ["Organization", "Public Services Division"]
+            },
+        ]
+    }).as('getTsm');
+
+    cy.intercept('GET', '/api/offices', {
+        statusCode: 200,
+        body: [
+            { id: 1, name: 'Organization' },
+            { id: 2, name: 'Public Services Division' },
+            { id: 3, name: 'Environmental Quality Division' },
+            { id: 4, name: "Green Areas, Parks and Animal Welfare Division" },
+            { id: 5, name: "Infrastructure Division" },
+            { id: 6, name: "General Services Division" }
+        ]
+    }).as('getOffices');
 };
 
 const stubCreateEmployee = () => {
@@ -68,16 +91,23 @@ const stubCreateCompany = () => {
     }).as('createCompany');
 };
 
+const stubUpdateTsmOffices = () => {
+    cy.intercept('PATCH', '/api/users/tsm/*', {
+        statusCode: 201,
+        body: { message: 'Technical Staff Member offices updated successfully!' },
+    }).as('updateTsmOffices');
+}
+
 const performLoginAsAdmin = () => {
     stubLoginAdmin();
     stubAdminPageData();
-    
+
     cy.visit(LOGINPAGE_URL);
-    
+
     loginPage.insertUsername('admin');
     loginPage.insertPassword('admin');
     loginPage.submitForm();
-    
+
     cy.wait('@loginAdmin');
     cy.wait(['@getOffices', '@getCompanies', '@getCategories']);
     cy.url().should('equal', ADMINPAGE_URL);
@@ -85,20 +115,14 @@ const performLoginAsAdmin = () => {
 
 describe("1. Test suite for the admin page (used to create new municipality users)", () => {
     beforeEach(() => {
-		cy.intercept('POST', '/api/users/me', { statusCode: 401, body: { message: 'Unauthorized' } }).as('me');
-	});
+        cy.intercept('POST', '/api/users/me', { statusCode: 401, body: { message: 'Unauthorized' } }).as('me');
+    });
 
     it('1.1 Logging in as an admin should redirect me to the admin page', () => {
         performLoginAsAdmin();
     });
 
-    it('1.2 As an admin, trying to visit another page, should redirect me to the admin page again', () => {
-        performLoginAsAdmin();
-        adminPage.clickHomepage();
-        cy.url().should('equal', ADMINPAGE_URL);
-    });
-
-    it('1.3 Create Municipal Administrator', () => {
+    it('1.2 Create Municipal Administrator', () => {
         performLoginAsAdmin();
         stubCreateEmployee();
 
@@ -106,7 +130,7 @@ describe("1. Test suite for the admin page (used to create new municipality user
         const ln = generateRandomString(10);
         const un = generateRandomString(15);
         const em = `${generateRandomString(12)}@participium.gov`;
-        
+
         adminPage.insertFirstName(fn);
         adminPage.insertLastName(ln);
         adminPage.insertUsername(un);
@@ -114,14 +138,14 @@ describe("1. Test suite for the admin page (used to create new municipality user
         adminPage.insertPassword("password");
         adminPage.selectRole(0);
         adminPage.submitAccount();
-        
+
         cy.wait('@createEmployee');
     });
 
-    it('1.4 Create Municipal Public Relations Officer', () => {
+    it('1.3 Create Municipal Public Relations Officer', () => {
         performLoginAsAdmin();
         stubCreateEmployee();
-        
+
         adminPage.insertFirstName(generateRandomString(10));
         adminPage.insertLastName(generateRandomString(10));
         adminPage.insertUsername(generateRandomString(15));
@@ -129,14 +153,14 @@ describe("1. Test suite for the admin page (used to create new municipality user
         adminPage.insertPassword("password");
         adminPage.selectRole(1);
         adminPage.submitAccount();
-        
+
         cy.wait('@createEmployee');
     });
 
-    it('1.5 Create Technical Office Staff Member', () => {
+    it('1.4 Create Technical Office Staff Member', () => {
         performLoginAsAdmin();
         stubCreateEmployee();
-        
+
         adminPage.insertFirstName(generateRandomString(10));
         adminPage.insertLastName(generateRandomString(10));
         adminPage.insertUsername(generateRandomString(15));
@@ -145,15 +169,15 @@ describe("1. Test suite for the admin page (used to create new municipality user
         adminPage.selectRole(2);
         adminPage.selectOffice(1);
         adminPage.submitAccount();
-        
+
         cy.wait('@createEmployee');
     });
 
-    it('1.6 Create new Company and then External Maintainer', () => {
+    it('1.5 Create new Company and then External Maintainer', () => {
         performLoginAsAdmin();
         stubCreateCompany();
         stubCreateEmployee();
-        
+
         adminPage.insertFirstName(generateRandomString(10));
         adminPage.insertLastName(generateRandomString(10));
         adminPage.insertUsername(generateRandomString(15));
@@ -162,7 +186,34 @@ describe("1. Test suite for the admin page (used to create new municipality user
         adminPage.selectRole(3);
         adminPage.selectCompany(2);
         adminPage.submitAccount();
-        
+
         cy.wait('@createEmployee');
+    });
+
+    it('1.6 Modify TSM offices, adding one of them', () => {
+        performLoginAsAdmin();
+
+        adminPage.moveToTsmManagement();
+
+        // now we have to select one of the existing technical office staff members :   
+        adminPage.selectFisrtTsm();
+        adminPage.selectFirstOffice();
+        stubUpdateTsmOffices();
+        adminPage.saveChanges();
+
+        cy.wait('@updateTsmOffices');
+    });
+
+    it('1.7 modify TSM offices, removing one of them', () => {
+        performLoginAsAdmin();
+
+        adminPage.moveToTsmManagement();
+        // now we have to select one of the existing technical office staff members :
+        adminPage.selectFisrtTsm();
+        adminPage.removeFirstOffice();
+        stubUpdateTsmOffices();
+        adminPage.saveChanges();
+
+        cy.wait('@updateTsmOffices');
     });
 })
